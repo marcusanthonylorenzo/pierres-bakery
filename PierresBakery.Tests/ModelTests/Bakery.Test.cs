0624a6de4671ca48d924163110e404cd7e8890eb
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-06 10:40:30 -0700

    pass second method test

--- PierresBakery.Tests/ModelTests/Bakery.Test.cs
+++ PierresBakery.Tests/ModelTests/Bakery.Test.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System;
 using Items;
 using MainBakery;
@@ -11,13 +12,31 @@ namespace Bakery.Tests
   {
 
     [TestMethod]
-     public void Welcome_TakesObjectsAsArguementsAndDisplaysTheirProperties_StringAndInt()
+    public void Welcome_TakesObjectsAsArguementsAndDisplaysTheirProperties_StringAndInt()
     {
       Bread loaf = new Bread();
       loaf.name = "Rye";
       loaf.price = 5;
-      Console.WriteLine($"1. {loaf.name} loaves");
+      Console.WriteLine($"1. {loaf.name} loaves at ${loaf.price} per loaf.");
       Assert.AreEqual(loaf.name, "Rye");
+      Assert.AreEqual(loaf.price, 5);
+    }
+
+    [TestMethod]
+    public void Buy_TakesInputAndChecksIfItemExistsAsKeyInFullMenuThenProceeds_StringAndInt()
+    {
+      Dictionary<string, int> fullMenu = new Dictionary<string, int>();
+      Bread loaf = new Bread();
+      loaf.name = "Rye";
+      loaf.price = 5;
+      fullMenu.Add(loaf.name, loaf.price);
+      //user input fields, change value of inputName to red, green check.
+      string inputName = "Rye";
+      if (!fullMenu.ContainsKey(inputName))
+      {
+        Console.WriteLine("Please enter a proper menu item, this item does not exist!");
+      }
+      Assert.AreEqual(fullMenu.ContainsKey(inputName), true);
     }
   }
 }
--- PierresBakery/Models/Bakery.cs
+++ PierresBakery/Models/Bakery.cs
@@ -7,17 +7,23 @@ namespace MainBakery
 {
     public class Shop
     {
+        public static Dictionary<string, int> fullMenu = new Dictionary<string, int>();
         public static void Main()
         {
             //Menu offering as if a real shop, used get/set because irl prices can change by the hour:
-            Bread loaf = new Bread();
-            loaf.name = "Rye";
-            loaf.price = 5;
+            Bread rye = new Bread();
+            rye.name = "Rye";
+            rye.price = 5;
+            fullMenu.Add(rye.name, rye.price);
             Pastry croissant = new Pastry();
             croissant.name = "Almond Croissant";
             croissant.price = 5;
+            fullMenu.Add(croissant.name, croissant.price);
+
             //Welcome!
-            Shop.Welcome(loaf, croissant);
+            Console.WriteLine(fullMenu.Keys);
+            Shop.Welcome(rye, croissant);
+            Shop.Buy();
 
         }
 
@@ -42,9 +48,24 @@ namespace MainBakery
             Console.WriteLine("**************************************************************");
             Console.WriteLine(" ");
             Console.WriteLine("Here's what we have to offer: ");
-            Console.WriteLine($"1. {menu1.name} loaves");
-            Console.WriteLine($"2. {menu2.name}s ");
+            Console.WriteLine($"1. {menu1.name} loaves at ${menu1.price} per loaf.");
+            Console.WriteLine($"2. {menu2.name}s at ${menu2.price} per pastry.");
             Console.WriteLine(" ");
+        }
+        
+        public static void Buy()
+        {
+            Console.WriteLine("Please enter the name of the item you would like:");
+            string inputName = Console.ReadLine();
+            if (!fullMenu.ContainsKey(inputName))
+            {
+                Console.WriteLine("Please enter a proper menu item, this item does not exist!");
+                Buy();
+            }
+            Console.WriteLine("Please enter the amount you would like: ");
+            int quantity = Int32.Parse(Console.ReadLine());
+ 
+            Console.WriteLine($"You've entered {inputName} at {quantity} units total.");
         }
     }
 }
